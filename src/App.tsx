36bf58620c3b248742cd492504c10e37de1578d7
Rui Saraiva <ruisaraiva19@gmail.com>
2021-11-18 09:22:35 +0000

    feat: setup Stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -20,7 +20,7 @@ function App() {
         </p>
         <p>
           <a
-            className="App-link"
+            className="app-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,7 +29,7 @@ function App() {
           </a>
           {' | '}
           <a
-            className="App-link"
+            className="app-link"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
