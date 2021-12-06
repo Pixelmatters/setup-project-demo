ba8355a34f8d39cbb2e4ad71e35a33005d7a7c3a
Rui Saraiva <ruisaraiva19@gmail.com>
2021-12-06 12:56:50 +0000

    ci: add CI/CD workflows (#1)

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  testMatch: ['**/src/**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
