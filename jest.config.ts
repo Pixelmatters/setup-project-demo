06f05890268e1a1b7529e656b7306278ae477237
Rui Saraiva <ruisaraiva19@gmail.com>
2023-08-01 16:31:45 +0100

    chore: clean up TS configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,16 @@
-module.exports = {
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+        diagnostics: false,
+      },
+    ],
+  },
   testMatch: ['**/src/**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -8,3 +19,5 @@ module.exports = {
   },
   testEnvironment: 'jest-environment-jsdom',
 }
+
+export default jestConfig
